ab1aa702a528a9971589cf86fe4e0ef7739cfde5
Stuart Stephen <stuart.stephen@gmail.com>
2017-07-19 18:34:24 +0100

    JDBC Driver no longer reports materialized views through the DatabaseMetadata.getTables() mechanism.

--- src/main/java/liquibase/ext/flexibleview/CreateFlexibleViewGenerator.java
+++ src/main/java/liquibase/ext/flexibleview/CreateFlexibleViewGenerator.java
@@ -1,19 +1,15 @@
 package liquibase.ext.flexibleview;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import liquibase.database.Database;
 import liquibase.database.core.OracleDatabase;
-import liquibase.database.jvm.JdbcConnection;
-import liquibase.exception.DatabaseException;
-import liquibase.exception.UnexpectedLiquibaseException;
 import liquibase.exception.ValidationErrors;
 import liquibase.ext.ora.dropmaterializedview.DropMaterializedViewOracle;
 import liquibase.ext.ora.dropmaterializedview.DropMaterializedViewStatement;
+import liquibase.ext.oracle.preconditions.OracleMaterializedViewExistsPrecondition;
 import liquibase.logging.LogFactory;
 import liquibase.logging.Logger;
 import liquibase.sql.Sql;
@@ -45,29 +41,13 @@ public class CreateFlexibleViewGenerator extends AbstractSqlGenerator<CreateFlex
 	 * 4. If the environment is set for materialized views, convert all views to materialized views at the end using the v99.99.99 script.
 	 */
 	public Sql[] generateSql( CreateFlexibleViewStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain ) {
-		
-		JdbcConnection connection = (JdbcConnection) database.getConnection();
 		// check if the view already exists, then formulate a plan from there!
-		
-		FlexibleView existingView = null;
-		
-		ResultSet rs = null;
-		try {
-			rs = connection.getMetaData().getTables( database.getLiquibaseCatalogName(), database.getLiquibaseSchemaName(), statement.getViewName().toUpperCase(), FlexibleView.getSQLValues() );
-			if ( rs.next() ) {
-				existingView = FlexibleView.deriveByValue( rs.getString( "TABLE_TYPE" ) );
-			}
-		} catch ( DatabaseException e ) {
-			throw new UnexpectedLiquibaseException( "Failed on database view investigation.", e );
-		} catch ( SQLException e ) {
-			throw new UnexpectedLiquibaseException( "Failed on database view investigation.", e );
-		} finally {
-			try { if ( rs != null ) rs.close(); } catch ( Throwable t ) { }
-		}
-		
+		String viewName = statement.getViewName().toUpperCase();
 		List<Sql> sequel = new ArrayList<Sql>();
-		
-		if ( FlexibleView.MATERIALIZED == existingView ) {
+
+		OracleMaterializedViewExistsPrecondition matViewExistsCondition = new OracleMaterializedViewExistsPrecondition();
+		matViewExistsCondition.setViewName( viewName );
+		if ( matViewExistsCondition.check( database ) ) {
 			DropMaterializedViewStatement dropMViewStmt = new DropMaterializedViewStatement( statement.getViewName() );
 			dropMViewStmt.setSchemaName( database.getLiquibaseSchemaName() );
 			DropMaterializedViewOracle dropMViewGen = new DropMaterializedViewOracle();
